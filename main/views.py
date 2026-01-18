48d713cda944e41188c70aa0c745806d368fcd6d
gabu <gabujr337@gmail.com>
2026-01-18 20:06:19 +0300

    removed the previous django app

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Resident
-
-# Create your views here.
-
-
-def resident_data(request):
-    data = Resident.objects.all()
-    return render(request, "index.html", {"residentData": data})

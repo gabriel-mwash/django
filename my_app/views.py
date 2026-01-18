67c1d5a8b19f3121f8b472197c510abf7222de0b
gabu <gabujr337@gmail.com>
2026-01-18 21:16:39 +0300

    removed everything to start a fresh

--- my_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("this is the home or index page")

a62fd72793aa7d8f7d84f471219886f0fc2322e3
gabu <gabujr337@gmail.com>
2026-01-29 23:53:08 +0300

    url request for my_app

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-        path("", views.greeting, name="greeting"),
+        path("", views.hello, name="home page")
         ]
